fbd1345877b54fb2e132b048fd13e095d2ae7c7a
Gabe Moya <gabemoya.dandelion090@aleeas.com>
2026-09-25 14:35:44 -0600

    .config/doom/config.el: rewrite

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,36 +1,44 @@
-;; :: a c i d c o m p u t i n g ::
+;; :: c a u s t i c  c o m p u t i n g ::
 ;; ~/.config/doom/config.el
+;; host: $HOSTNAME
+;; DOOM Emacs configuration file
+;; created <2026-09-25> :: last modified <2026-09-25T14:33-06:00>
+;;
+;; --- BEGIN CONFIGURATION FILE ---
 
-;; --- F o n t s ---
+;; :: fonts ::
 (setq doom-font                (font-spec :family "JetBrains Mono Slashed" :size 17)
       doom-variable-pitch-font (font-spec :family "Inter" :size 15)
       doom-serif-font          (font-spec :family "FreeSerif" :size 19)
       doom-symbol-font         (font-spec :family "JetBrains Mono Slashed" :size 17)
       doom-big-font            (font-spec :family "JetBrains Mono Slashed" :size 24))
 
-;; --- T h e m e s ---
+;; :: theme ::
 (setq doom-theme 'doom-outrun-electric)
 
-;; --- L i n e  N u m b e r s ---
+;; :: line numbers ::
 (setq display-line-numbers-type t)
 
-;; --- E d i t o r ---
+;; :: editor ::
 ;; 2 space tabs, no tabs.
 (setq-default tab-width 2
               indent-tabs-mode nil)
-;; --- S c r o l l i n g ---
+
+;; :: scrolling ::
 (setq scroll-preserve-screen-position t)
 
-;; --- A u t o s a v e 
+;; :: autosave :: 
 (setq auto-save-default nil)
 
-;; --- V t e r m ---
+;; :: vterm ::
 (setq vterm-shell "/usr/bin/zsh")
 (setq vterm-max-scrollback 10000)
 (setq vterm-timer-delay 0.01)
 
-;; --- O r g  M o d e ---
-(setq org-directory "~/SyncPool/DBs/PKMS/Org")
+;; :: org mode ::
+(setq org-directory "~/SyncPool/DBs/PKMS/Master/Org")
 
-;; --- F r a m e ---
+;; :: org mode ::
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; --- END CONFIGURATION FILE ---
